f781965281122eda06a31acf01caca8014e0db29
frauomnia <omnia.mohamad@gmail.com>
2025-03-17 17:47:37 +0100

    fix wait for params error

--- src/app/volunteers/[id]/page.tsx
+++ src/app/volunteers/[id]/page.tsx
@@ -1,4 +1,5 @@
 import DeleteButton from '@/components/DeleteButton';
+import Navbar from '@/components/Navbar';
 import prisma from '@/lib/db/prisma';
 import Image from 'next/image'
 import { notFound } from 'next/navigation';
@@ -10,15 +11,19 @@ interface VolunteerPageProps {
 }
 
 export default async function VolunteerPage( { params } : VolunteerPageProps) 
-{
+{   
+    /* due to an error to await params before 
+    using them as paramas are now treated as a Promise */
+    const { id } = await params;
     const volunteer = await prisma.volunteer.findUnique({
-        where: {id: params.id}
+        where: { id }
     })
 
     if(!volunteer) notFound();
 
     return(
         <div>
+            <Navbar/>
             <div className="flex flex-col">
                 <Image 
                     src="/images/avatar-placeholder.png"
