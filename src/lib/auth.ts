a4b7bd6efd8bd7b99938efe96dc530b6dd8a55a6
frauomnia <omnia.mohamad@gmail.com>
2025-03-22 20:17:21 +0100

    prisma schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,24 +1,24 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { prisma } from "./db/prisma"
-export const { auth, handlers } = NextAuth({ providers: 
+// import NextAuth from "next-auth"
+// import Credentials from "next-auth/providers/credentials"
+// import { prisma } from "./db/prisma"
+// export const { auth, handlers } = NextAuth({ providers: 
     
-    [Credentials({
-        credentials: {
-            email: {},
-            password: {},
-            name: {}
-        },
-        authorize:async (credentials) => {
-            const user = await prisma.user.findFirst({
-                where: {email: credentials.email, password: credentials.password},
-            });
-            if (!user) {
-                throw new Error("Invalid credentials.");
-            }
-            return user;
-        }
-    })
-    ] 
-})
+//     [Credentials({
+//         credentials: {
+//             email: {},
+//             password: {},
+//             name: {}
+//         },
+//         authorize:async (credentials) => {
+//             const user = await prisma.user.findFirst({
+//                 where: {email: credentials.email, password: credentials.password},
+//             });
+//             if (!user) {
+//                 throw new Error("Invalid credentials.");
+//             }
+//             return user;
+//         }
+//     })
+//     ] 
+// })
 
