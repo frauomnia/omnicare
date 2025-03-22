9c063c8c710090e6ee6229a27e8523bda11fa85a
frauomnia <omnia.mohamad@gmail.com>
2025-03-22 10:56:49 +0100

    re-arrange routes based on user roles, re-add admin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import CarouselDisplay from "@/components/CarouselDisplay";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
 
@@ -12,56 +13,8 @@ export default function Home() {
       <div className="btn w-[30%] ml-auto mr-auto">
         <Link className="ml-5 mt-5" href="/volunteers/">Find your doctor</Link>
       </div>
-      <div className="carousel carousel-center bg-[#F1E6D0] rounded-box w-[70%] space-x-4 p-4 mt-8 ml-auto mr-auto">
-        <div className="carousel-item w-[100%] relative">
-          <div className="absolute">
-            <p>Our mission</p>
-            <p>Our goal is to help refugees find medical volunteers.</p>
-          </div>
-          <img
-            src="/images/avocado.jpg"
-            className="rounded-box" />
-        </div>
-        <div className="carousel-item w-[100%] relative">
-        <div className="absolute">
-        <p>Our mission</p>
-            <p>Our goal is to help break language barrier between patient and medical care provider.</p>
-          </div>
-          <img
-            src="/images/one-finger.jpg"
-            className="rounded-box" />
-        </div>
-        <div className="carousel-item w-[100%] relative">
-        <div className="absolute">
-        <p>Our mission</p>
-            <p>Our goal is to help break cultural barrier between patient and medical care provider.</p>
-          </div>
-          <img
-            src="/images/two-thumbs.jpg"
-            className="rounded-box" />
-        </div>
-        <div className="carousel-item w-[100%] relative">
-        <div className="absolute">
-        <p>Our mission</p>
-            <p>Our goal is to help refugees regain power with medical care.</p>
-          </div>
-          <img
-            src="/images/power.jpg "
-            className="rounded-box" />
-        </div>
-        <div className="carousel-item w-[100%] relative">
-        <div className="absolute">
-            <p>Our mission</p>
-            <p>Our goal is to help spread peace and love.</p>
-
-          </div>
-          <img
-            src="/images/peace.jpg"
-            className="rounded-box" />
-        </div>
+      <CarouselDisplay />
       </div>
-      </div>
-     
    </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,8 +23,9 @@ export default function Navbar() {
                             className="ml-4"
                         />
                         <Link className="ml-5 mt-5" href='/'>Home</Link>
-                        {/* <Link className="ml-5 mt-5" href="/register/">Register</Link> */}
-                        {/* <Link className="ml-5 mt-5" href="/log-in/">Log in</Link> */}
+                        <Link className="ml-5 mt-5" href="/volunteers/register/">Register</Link>
+                        <Link className="ml-5 mt-5" href="/admin/">Admin view</Link>
+                        <Link className="ml-5 mt-5" href="/sign-in/">Log in</Link>
                     </div>
                 </div>
             </div>
