c2eea86e9568eecb028f285db40e5101e8e8bae0
frauomnia <omnia.mohamad@gmail.com>
2025-03-25 13:01:07 +0100

    add more styles to home page

--- src/components/CarouselDisplay.tsx
+++ src/components/CarouselDisplay.tsx
@@ -8,7 +8,7 @@ return (
   <div className="carousel carousel-center bg-[#F1E6D0] rounded-box w-[70%] space-x-4 p-4 mt-8 ml-auto mr-auto">
     <div className="carousel-item w-[100%] relative">
       <div className="absolute text-[#F1E6D0] m-10 font-bold text-xl">
-        <p className="">Our mission</p>
+        <p className="underline mb-4">Our mission</p>
         <p>Our goal is to help refugees find medical volunteers.</p>
       </div>
       <img
@@ -18,7 +18,7 @@ return (
     </div>
     <div className="carousel-item w-[100%] relative">
     <div className="absolute text-[#48752C] m-10 font-bold text-xl">
-        <p>Our mission</p>
+    <p className="underline mb-4">Our mission</p>
         <p>Our goal is to help break language barrier between patient and medical care provider.</p>
     </div>
     <img
@@ -28,7 +28,7 @@ return (
     </div>
     <div className="carousel-item w-[100%] relative">
     <div className="absolute text-[#48752C] m-10 font-bold text-xl">
-      <p>Our mission</p>
+    <p className="underline mb-4">Our mission</p>
       <p>Our goal is to help break cultural barrier between patient and medical care provider.</p>
     </div>
     <img
@@ -38,7 +38,7 @@ return (
     </div>
     <div className="carousel-item w-[100%] relative">
     <div className="absolute text-[#48752C] m-10 font-bold text-xl">
-      <p>Our mission</p>
+    <p className="underline mb-4">Our mission</p>
       <p>Our goal is to help refugees regain power with medical care.</p>
       </div>
       <img
@@ -48,7 +48,7 @@ return (
     </div>
     <div className="carousel-item w-[100%] relative">
     <div className="absolute text-[#48752C] m-10 font-bold text-xl">
-      <p>Our mission</p>
+    <p className="underline mb-4">Our mission</p>
       <p>Our goal is to help refugees have a peaceful life again.</p>
     </div>
     <img
